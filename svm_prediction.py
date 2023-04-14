b88f0c994f970342be9219f04d2d025d5bc1940b
Navein Suresh <naveinksuresh@gmail.com>
2023-04-13 23:41:26 -0700

    New Changes

--- svm_prediction.py
+++ svm_prediction.py
@@ -1,5 +1,9 @@
-from sklearn.svm import SVC
+from sklearn.svm import SVR
 from sklearn.model_selection import RandomizedSearchCV
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class SVMPrediction:
 
@@ -12,27 +16,40 @@ class SVMPrediction:
     uses random_search to optimize hyperparameters for transformation matrix
     """
 
-    def __init__(self, kernel='rbf', C=1.0, gamma='scale'):
+    def __init__(self, x_train, y_train, kernel='rbf', C=1.0):
         # initializes hyperparameters for SVM classifer
         self.kernel = kernel
         self.C = C
-        self.gamma = gamma
-        self.classifier = SVC(kernel=self.kernel, C=self.C, gamma=self.gamma)
+        self.classifier = SVR(kernel=self.kernel, C=self.C)
         self.w = 0
-        
-    def train(self, x_train, y_train):
+        self.x_train = np.array(x_train)
+        self.y_train = np.array(y_train)
+
+    def train(self):
         # determines the ideal transformation matrix for prediction
         # performs a random_search operation to optimize appropriate hyperparameters for w
         distribution = {'C': [0.1, 1, 10, 100],
-                      'gamma': ['scale', 'auto', 0.1, 0.01, 0.001, 0.0001],
                       'kernel': ['linear', 'rbf', 'poly', 'sigmoid']}
         random_search = RandomizedSearchCV(self.classifier, param_distributions=distribution, cv=5, n_iter=10)
-        random_search.fit(x_train, y_train)
+        random_search.fit(self.x_train, self.y_train)
         self.classifier = random_search.best_estimator_
-        self.w = self.classifier.coef_
-        return self.w
+        
         
     def predict(self, x_test):
         # predicts output data given test data
         return self.classifier.predict(x_test)
     
+# Load the data from the csv file
+data = pd.read_csv('training_data.csv')
+x_train = data[['Correct', 'Total']].values
+y_train = data['Cluster Rating'].values
+
+# Create and train the model
+model = SVMPrediction(x_train, y_train)
+model.train()
+
+# Predict the y for the given x values
+x_pred = [[1, 2], [2, 2], [3, 5], [5, 5]]
+y_pred = model.predict(x_pred)
+print(y_pred)
+        
